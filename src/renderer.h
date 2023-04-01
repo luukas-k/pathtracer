d29ae8eafe9d10db905e2a9135967ca53a4d9c4a
luukas-k <25417369+luukas-k@users.noreply.github.com>
2023-04-01 21:30:00 +0300

    Update renderer
    Removed recursion from scene raycasting. And increase render block size to 64x64.

--- src/renderer.h
+++ src/renderer.h
@@ -10,12 +10,18 @@
 #include <chrono>
 #include <functional>
 
-std::pair<u32, u32> minutes_and_seconds(u32 seconds) {
-	return { seconds / 60, seconds - (seconds / 60) * 60 };
+std::tuple<u32, u32, u32> hours_minutes_and_seconds(u32 seconds) {
+	u32 hrs = seconds / 3600;
+	seconds -= hrs * 3600;
+	u32 mins = seconds / 60;
+	seconds -= mins * 60;
+	return { hrs, mins, seconds};
 }
 
 std::string time_string(u32 seconds) {
-	auto [mins, sec] = minutes_and_seconds(seconds);
+	auto [hrs, mins, sec] = hours_minutes_and_seconds(seconds);
+	if(hrs > 0)
+		return std::to_string(hrs) + "hr " + std::to_string(mins) + "min " + std::to_string(sec) + "s";
 	if (mins > 0)
 		return std::to_string(mins) + "min " + std::to_string(sec) + "s";
 	return std::to_string(sec) + "s";
@@ -31,7 +37,7 @@ public:
 			threads.push_back(std::thread([this]() {
 				bool should_terminate = false;
 				do {
-					if(terminate){
+					if (terminate) {
 						should_terminate = true;
 						break;
 					}
@@ -88,6 +94,10 @@ public:
 		std::lock_guard l(task_mutex);
 		return (u32)task_queue.size();
 	}
+	u32 thread_count() const {
+		std::lock_guard l(task_mutex);
+		return threads.size();
+	}
 private:
 	mutable std::mutex task_mutex;
 	std::vector<Task> task_queue;
@@ -135,66 +145,6 @@ public:
 		number_t ar = (number_t)w / (number_t)h;
 		u32 last_completion = 0;
 
-		// Render function that renders from start_y to start_y + height slice of the image
-		auto compute = [&](u32 start_y, u32 height) {
-			auto t1 = std::chrono::high_resolution_clock::now();
-			u32 completion = 0;
-			for (u32 y = start_y; y < start_y + height; y++) {
-				auto t2 = std::chrono::high_resolution_clock::now();
-				auto elapsed = (t2 - t1);
-				auto cur_completion = (u32)((number_t)(y - start_y) / (number_t)(height) * 100.f);
-				if (cur_completion > completion) {
-					u32 elapsed_s = (u32)std::chrono::duration_cast<std::chrono::seconds>(elapsed).count();
-					std::cerr <<
-						"t[" << std::this_thread::get_id() << "]:" <<
-						"Percentage: " << cur_completion <<
-						"\tElapsed: " << time_string(elapsed_s) << "s" <<
-						"\tThread remaining: " << time_string((u32)(((number_t)elapsed_s / (number_t)cur_completion) * 100.f) - elapsed_s) << "s\n";
-					completion = cur_completion;
-				}
-				for (u32 x = 0; x < w; x++) {
-					Vec3 col{ 0.f, 0.f, 0.f };
-					for (u32 sample = 0; sample < samples; sample++) {
-						number_t u = x + random_f32();
-						number_t v = y + random_f32();
-						col = col + raycast_scene(scene, cam.get_ray(u, v, (number_t)w, (number_t)h, ar), bounces + 1);
-					}
-					rt.get(x, y) = sqrt(col * (1.f / samples));
-				}
-			}
-		};
-
-		struct Task {
-			u32 start_y;
-			u32 height;
-		};
-
-		std::vector<std::thread> tasks;
-
-		// Generate render tasks for threads
-		// A single render task renders a slice of the image starting from start_y to start_y + height
-		for (u32 n = 0; n < num_threads; n++) {
-			u32 start = n * (h / num_threads);
-			u32 height = (n + 1) * (h / num_threads) - start;
-			if (n == num_threads - 1)
-				height = h - start;
-			std::cerr << start << ":" << height << "\n";
-			tasks.push_back(std::thread(compute,
-										start,
-										height
-			));
-		}
-
-		// Wait for rendering to complete
-		for (auto& task : tasks) {
-			task.join();
-		}
-	}
-	void render_mt_new(const Scene& scene, const Camera& cam, Image& rt) {
-		u32 w = rt.width(), h = rt.height();
-		number_t ar = (number_t)w / (number_t)h;
-		u32 last_completion = 0;
-
 		struct TaskInfo {
 			u32 x, y, w, h;
 		};
@@ -202,9 +152,9 @@ public:
 		ThreadPool tp(num_threads);
 		u32 total_tasks = 0;
 		for (u32 y = 0; y < h; ) {
-			u32 cons_h = min<u32>(8, h - y);
+			u32 cons_h = min<u32>(64, h - y);
 			for (u32 x = 0; x < w; ) {
-				u32 cons_w = min<u32>(8, w - x);
+				u32 cons_w = min<u32>(64, w - x);
 				TaskInfo ti{
 					.x = x,
 					.y = y,
@@ -233,7 +183,7 @@ public:
 		auto t0 = std::chrono::high_resolution_clock::now();
 		while (!tp.is_done()) {
 			using namespace std::chrono_literals;
-			std::this_thread::sleep_for(1s);
+			std::this_thread::sleep_for(2s);
 			u32 num_tasks_left = tp.task_queue_size();
 			auto t1 = std::chrono::high_resolution_clock::now();
 			auto time_since_start = (u32)std::chrono::duration_cast<std::chrono::seconds>(t1 - t0).count();
@@ -254,7 +204,8 @@ public:
 	void set_thread_count(u32 n) { num_threads = n; }
 	u32 get_thread_count() const { return num_threads; }
 private:
-	Vec3 raycast_scene(const Scene& scene, Ray ray, i32 depth) {
+	/*
+	Vec3 raycast_scene_recurse(const Scene& scene, Ray ray, i32 depth) {
 		// If maximum depth is reached no light happens
 		if (depth <= 0)
 			return Vec3{ 0.f, 0.f, 0.f };
@@ -291,8 +242,8 @@ private:
 				Vec3 scatter_dir = reflect(ray.direction().normalize(), normal.normalize());
 				if (!cannot_refract)
 					scatter_dir = refract(ray.direction().normalize(), normal.normalize() * (is_front ? -1.f : 1.f), refraction_ratio);
-
-				scatter_dir = scatter_dir + random_in_unit_sphere() * 0.1f;
+				// else
+				// 	scatter_dir = scatter_dir + random_in_unit_sphere() * 0.1f;
 
 				return mat->d.emissive + raycast_scene(scene, Ray(pos, scatter_dir).advance(REFLECTION_ADVANCE), depth - 1);
 			}
@@ -300,6 +251,70 @@ private:
 		// Nothing was hit so no light is accumulated
 		return Vec3{ 0.f, 0.f, 0.f };
 	}
+	*/
+	Vec3 raycast_scene(const Scene& scene, Ray ray, i32 max_depth) {
+		Vec3 color{0.f, 0.f, 0.f};
+		Vec3 factor{1.f, 1.f, 1.f};
+
+		for (i32 depth = 0; depth < max_depth; depth++) {
+			// Ray march into the scene to find the ray scene intersection
+			auto pos_or = scene.ray(ray, path_step_max, EPSILON);
+
+			// If there is a collision do shading computations
+			if (auto pos = pos_or.value_or(Vec3{}); pos_or.has_value()) {
+				auto [distance_to_scene, normal, mat] = scene.distance_and_normal_and_material(pos);
+
+				// Advance the reflection ray a bit to reduce self intersection of the ray
+				// Different scattering if the material is a metal, lambertian or dielectric
+				number_t REFLECTION_ADVANCE = EPSILON * 1.2f;
+				if (mat->type == MaterialType::Metallic) {
+					Vec3 scatter_dir = reflect(ray.direction(), normal) + random_in_unit_sphere() * (1.f - mat->m.shininess);
+					color = color + factor * mat->m.emissive;
+
+					factor = {1.f, 1.f, 1.f};
+					ray = Ray(pos, scatter_dir).advance(REFLECTION_ADVANCE);
+				}
+				else if (mat->type == MaterialType::Lambertian) {
+					Vec3 target = pos + normal + random_in_unit_sphere();
+					Vec3 scatter_dir = target - pos;
+
+					color = color + factor * mat->l.emissive;
+
+					factor = mat->l.albedo * mat->l.reflectance;
+					ray = Ray(pos, scatter_dir).advance(REFLECTION_ADVANCE);
+				}
+				else if (mat->type == MaterialType::Dielectric) {
+					bool is_front = dot(ray.direction(), -normal.normalize()) < 0.f;
+					number_t refraction_ratio = is_front ? mat->d.refractive_index : 1.f / mat->d.refractive_index;
+					// Check if internal or external refraction
+
+					double cos_theta = fmin(dot(-ray.direction(), normal), 1.0);
+					double sin_theta = sqrt(1.0 - cos_theta * cos_theta);
+
+					bool cannot_refract = refraction_ratio * sin_theta > 1.0;
+
+					Vec3 scatter_dir = reflect(ray.direction().normalize(), normal.normalize());
+					if (!cannot_refract)
+						scatter_dir = refract(ray.direction().normalize(), normal.normalize() * (is_front ? -1.f : 1.f), refraction_ratio);
+
+					scatter_dir = scatter_dir + random_in_unit_sphere() * 0.1f;
+
+					color = color + factor * mat->d.emissive;
+
+					factor = {1.f, 1.f, 1.f},
+					ray = Ray(pos, scatter_dir).advance(REFLECTION_ADVANCE);
+				}
+				else {
+					// Unkown mat
+				}
+			}
+			else {
+				break;
+			}
+		}
+		return color;
+	}
+
 	u32 samples = 4;
 	u32 bounces = 4;
 	u32 path_step_max = 100;
